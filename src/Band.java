e2219bf8c16ba669ca5995a0834a6d8ffab20ba1
Naya Schwartz <schwartznaya@gmail.com>
2026-09-23 09:26:54 +0200

    todo list first commit

---
+++ src/Band.java
@@ -0,0 +1,15 @@
+public class Band {
+
+    private char[] genre = {'r','e','h','p'};
+
+    private String bandName;
+    private int fans;
+    private int maxFans;
+    private int level;
+    private int xP;
+    private double money;
+    private boolean isActive;
+
+    //todo: band name print, Addfans, losefans, level up, addMoney, loseMoney, !isActive,
+
+}
---
+++ src/Repertoire.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Repertoire {
+
+    ArrayList<String> songs = new ArrayList<>();
+
+    //todo: addsong method, printsongs, print individual song (toString), assign to band - band HAS A repertoire
+
+
+}
